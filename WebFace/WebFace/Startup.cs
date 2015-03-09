bfee37e101622fe697b6bce28ac70fe1b5ae807a
fabianvaccaro <fabianvaccaro@hotmail.com>
2015-03-09 22:22:35 +0100

    first committ

---
+++ WebFace/WebFace/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebFace.Startup))]
+namespace WebFace
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
